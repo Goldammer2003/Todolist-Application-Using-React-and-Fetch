f85b4a1f6d3cfa9826a82c09c1b911da97c6a014
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-17 19:31:25 +0000

    Great Progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+
+
+const [todos,setTodos] =useState ([])
+console.log (todos)
+  return (
+    <div className="text-center">
+      <h1> ToDoList</h1>
+      <input
+        type="text"
+        onKeyPress={(e) => {
+          if (e.key =="Enter") {
+			setTodos ([...todos,inputValue])
+			setInputValue ("")
+		  }
+        }}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        value={inputValue}
+      ></input>
+	  <ul className="list-group">
+		{todos.map((item)=>{
+			return (
+				<li className="list-group-item">{item}</li>
+			)
+		})}
+	  </ul>
+    </div>
+  );
 };
 
 export default Home;
