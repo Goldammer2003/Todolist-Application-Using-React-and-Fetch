2c1877c698dd513c2e34789131bfeccaf95c6278
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-26 17:31:20 +0000

    Big progress done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -11,6 +11,13 @@ const Home = () => {
   const [isShown, setIsShown] = useState({ state: false, index: 0 });
   const [todos, setTodos] = useState([]);
   console.log(todos);
+  useEffect (()=> {
+    getTodos ()
+  },[])
+
+  useEffect (()=>{
+    saveTodos ()
+  } ,[todos] )
 
   const deletetodo = (index) => {
   const newlistafterdeletion =todos.filter ((item,i)=>{
@@ -18,6 +25,24 @@ const Home = () => {
   })  
   setTodos (newlistafterdeletion)
   }
+
+const getTodos = () => {
+  fetch ('https://assets.breatheco.de/apis/fake/todos/user/Goldammer2003')
+  .then (response =>response.json () ) 
+  .then (data => setTodos(data))
+}
+
+const saveTodos = () => {
+  fetch ('https://assets.breatheco.de/apis/fake/todos/user/Goldammer2003',{
+    method:'PUT', 
+    headers: {
+      'Content-Type': 'application/json'
+
+    },
+    body:JSON.stringify (todos)
+  })
+}
+
   return (
     <div className="text-center">
       <h1> ToDoList</h1>
@@ -28,7 +53,10 @@ const Home = () => {
         type="text"
         onKeyPress={(e) => {
           if (e.key == "Enter") {
-            setTodos([...todos, inputValue]);
+            setTodos([...todos, {
+              label:inputValue, 
+              done:false
+            }]);
             setInputValue("");
           }
         }}
@@ -39,11 +67,13 @@ const Home = () => {
       ></input></li>
         {todos.map((item, ind) => {
          
+
+
           return (
             <li className="list-group-item w-25 m-auto" onMouseEnter={() => setIsShown({ state: true, index: ind })}   onMouseLeave={() => setIsShown({ state: false, index: 0 })}>
               {" "}
               <div className ="d-flex justify-content-between container">
-                <span>{item}</span>
+                <span>{item.label}</span>
                 {isShown.state == true && isShown.index == ind ? <button type="button" className="btn btn-danger " onClick = {()=>deletetodo(ind)}>
                   <i className="fas fa-trash"></i>
                 </button>: ''}
