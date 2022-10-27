be11687506d6ed65f9cacb0066f3d0661bb5fd0f
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-27 17:52:44 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-  const [inputValue, setInputValue] = useState("No tasks, add a task");
+  const [inputValue, setInputValue] = useState("");
 
 
   const [isShown, setIsShown] = useState({ state: false, index: 0 });
@@ -38,6 +38,8 @@ const saveTodos = () => {
     headers: {
       'Content-Type': 'application/json'
 
+
+
     },
     body:JSON.stringify (todos)
   })
@@ -48,7 +50,9 @@ const saveTodos = () => {
       <h1> ToDoList</h1>
      
       <ul className="list-group">
-        <li className="list-group-item w-25 m-auto"> <input
+        <li className="list-group-item w-25 m-auto">
+           <input
+           placeholder='Add a task'
         className ="w-75"
         type="text"
         onKeyPress={(e) => {
@@ -74,7 +78,9 @@ const saveTodos = () => {
               {" "}
               <div className ="d-flex justify-content-between container">
                 <span>{item.label}</span>
-                {isShown.state == true && isShown.index == ind ? <button type="button" className="btn btn-danger " onClick = {()=>deletetodo(ind)}>
+                {isShown.state == true && isShown.index == ind ? <button type="button" className="btn btn-danger " onClick = {()=>{deletetodo(ind)
+              saveTodos ()
+                }}>
                   <i className="fas fa-trash"></i>
                 </button>: ''}
               </div>
